b7642e6cf1cddb7cf46122489dc4019721847db4
Jeffry Kamsteeg Rodiño <j.kamsteeg1570@pascualbravo.edu.co>
2023-08-12 23:02:32 -0500

    Add a synchrous bcrypt example

--- server.js
+++ server.js
@@ -15,24 +15,21 @@ app.get('/', (req, res) => {
 });
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     console.log(hash);
     bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
         console.log(res)
     });
 });
 
-
-
-
-
-
-
-
 //END_ASYNC
 
 //START_SYNC
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
 
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 
 //END_SYNC
